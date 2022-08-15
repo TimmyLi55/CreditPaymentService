a5c150a930551fc258f41b6b09b85c11fc8b8d52
TimmyLi <artelaw@gmail.com>
2022-08-16 01:25:10 +0600

    Исправления

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
         CreditPaymentService CrPaSe = new CreditPaymentService();
         System.out.println("Первый скрин, ваш ежемесячный платеж:");
         System.out.println((CrPaSe.calculate(1_000_000, 9.99F, 1)) + " руб.");
-        System.out.println("Первый скрин, ваш ежемесячный платеж:");
+        System.out.println("Второй скрин, ваш ежемесячный платеж:");
         System.out.println((CrPaSe.calculate(1_000_000, 9.99F, 2)) + " руб.");
-        System.out.println("Первый скрин, ваш ежемесячный платеж:");
+        System.out.println("Третий скрин, ваш ежемесячный платеж:");
         System.out.println((CrPaSe.calculate(1_000_000, 9.99F, 3)) + " руб.");
 
     }
